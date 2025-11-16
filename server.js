3e6b2c0e2e6043aaf63cb7ff1ae225cab76f5de3
Agam715 <tanuska981@gmail.com>
2025-11-16 19:55:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,44 +6,34 @@ const app = express();
 // Create folders
 if (!fs.existsSync("hls")) fs.mkdirSync("hls");
 if (!fs.existsSync("hls/ss2")) fs.mkdirSync("hls/ss2");
-if (!fs.existsSync("hls/ch2")) fs.mkdirSync("hls/ch2");
 
-// CHANNEL LIST
-const CHANNELS = {
-  ss2: "http://87.255.35.150:18828",
-  ch2: "http://rkdyiptv-yupp24.wasmer.app/play.php?id=2756"
-};
+// CHANNEL source
+const STREAM = "http://87.255.35.150:18828";  // Star Sports 2 TS stream
 
-function startFFmpeg(name, url) {
-  console.log("Starting channel:", name);
+// Start FFmpeg
+console.log("Starting Star Sports 2...");
+const ffmpeg = spawn("ffmpeg", [
+  "-i", STREAM,
+  "-c:v", "copy",
+  "-c:a", "copy",
+  "-f", "hls",
+  "-hls_time", "2",
+  "-hls_list_size", "10",
+  "-hls_flags", "delete_segments",
+  "hls/ss2/playlist.m3u8"
+]);
 
-  const ffmpeg = spawn("ffmpeg", [
-    "-i", url,
-    "-c:v", "copy",
-    "-c:a", "copy",
-    "-f", "hls",
-    "-hls_time", "2",
-    "-hls_list_size", "10",
-    "-hls_flags", "delete_segments",
-    `hls/${name}/playlist.m3u8`
-  ]);
-
-  ffmpeg.stderr.on("data", d => console.log(`[${name}]`, d.toString()));
-  ffmpeg.on("close", () => console.log(`Channel stopped: ${name}`));
-}
-
-// Start all channels
-Object.entries(CHANNELS).forEach(([name, url]) => startFFmpeg(name, url));
+ffmpeg.stderr.on("data", d => console.log("[ss2]", d.toString()));
+ffmpeg.on("close", () => console.log("Channel stopped"));
 
 // Serve HLS
 app.use("/hls", express.static("hls"));
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>Channels Running</h1>
+    <h1>Channel Running</h1>
     <ul>
       <li><a href="/hls/ss2/playlist.m3u8">Star Sports 2</a></li>
-      <li><a href="/hls/ch2/playlist.m3u8">Channel 2756</a></li>
     </ul>
   `);
 });
