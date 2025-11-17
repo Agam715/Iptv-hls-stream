c7dee6649ccd933b5f563ba3c134b31081940c2c
Agam715 <tanuska981@gmail.com>
2025-11-17 19:20:30 +0530

    Refactor channel configuration and folder creation

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const { spawn } = require("child_process");
 const fs = require("fs");
 const app = express();
 
-// ----------------------------
-// ENABLE CORS FOR GITHUB PAGES
-// ----------------------------
+// ⭐ ENABLE CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
@@ -13,62 +11,66 @@ app.use((req, res, next) => {
   next();
 });
 
-// ----------------------------
-// CHANNEL LIST (YOU OWN THESE)
-// ----------------------------
+// Ensure HLS folder exists
+if (!fs.existsSync("hls")) fs.mkdirSync("hls");
+
+// CHANNELS
 const CHANNELS = {
-  ss2:      "http://87.255.35.150:18828",                 // Star Sports 2
-  ten3:     "http://87.255.35.150:18848",                 // Sony Ten 3 Hindi
-  ss1_4k:   "http://sports-rkdyiptv.wasmer.app/play.php?id=71242"  // Star Sports 1 4K (YOU OWN)
+  ss2: {
+    url: "http://87.255.35.150:18828",
+    folder: "hls/ss2"
+  },
+  ten3: {
+    url: "http://87.255.35.150:18848",
+    folder: "hls/ten3"
+  },
+  ss1_4k: {
+    url: "http://sports-rkdyiptv.wasmer.app/play.php?id=71242",
+    folder: "hls/ss1_4k"
+  }
 };
 
-// ----------------------------
-// MAKE HLS DIRECTORIES
-// ----------------------------
-if (!fs.existsSync("hls")) fs.mkdirSync("hls");
-for (let ch in CHANNELS) {
-  if (!fs.existsSync(`hls/${ch}`)) fs.mkdirSync(`hls/${ch}`);
+// Create folder if not exist
+function ensureFolder(path) {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path, { recursive: true });
+    console.log("Created folder:", path);
+  }
 }
 
-// ----------------------------
-// START FFMPEG FOR EACH CHANNEL
-// ----------------------------
-function startChannel(name, url) {
-  console.log(`Starting channel: ${name}`);
+function startChannel(key, data) {
+  ensureFolder(data.folder);
+
+  console.log("Starting channel:", key);
 
   const ffmpeg = spawn("ffmpeg", [
-    "-i", url,
+    "-i", data.url,
     "-c:v", "copy",
     "-c:a", "copy",
     "-f", "hls",
     "-hls_time", "2",
     "-hls_list_size", "10",
     "-hls_flags", "delete_segments",
-    `hls/${name}/playlist.m3u8`
+    `${data.folder}/playlist.m3u8`
   ]);
 
-  ffmpeg.stderr.on("data", data => {
-    console.log(`[${name}] ${data}`);
-  });
-
-  ffmpeg.on("close", () => {
-    console.log(`FFmpeg stopped for: ${name}`);
-  });
+  ffmpeg.stderr.on("data", d => console.log(`[${key}]`, d.toString()));
+  ffmpeg.on("close", () => console.log(`Channel stopped: ${key}`));
 }
 
-Object.entries(CHANNELS).forEach(([name, url]) => startChannel(name, url));
+// Start all channels
+Object.entries(CHANNELS).forEach(([key, data]) => startChannel(key, data));
 
-// ----------------------------
-// STATIC FILE SERVING
-// ----------------------------
+// Serve HLS
 app.use("/hls", express.static("hls"));
 
+// Status page
 app.get("/", (req, res) => {
   res.send(`
     <h1>Channels Running</h1>
     <ul>
       <li><a href="/hls/ss2/playlist.m3u8">Star Sports 2</a></li>
-      <li><a href="/hls/ten3/playlist.m3u8">Sony Ten 3 Hindi</a></li>
+      <li><a href="/hls/ten3/playlist.m3u8">Sony TEN 3 Hindi</a></li>
       <li><a href="/hls/ss1_4k/playlist.m3u8">Star Sports 1 4K Hindi</a></li>
     </ul>
   `);
