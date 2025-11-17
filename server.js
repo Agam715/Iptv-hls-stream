53a7aa1e63b2aa63349a431c241075b07f401fd0
Agam715 <tanuska981@gmail.com>
2025-11-17 19:13:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { spawn } = require("child_process");
 const fs = require("fs");
 const app = express();
 
-// ⭐ ENABLE CORS FOR GITHUB PAGES ⭐
+// ----------------------------
+// ENABLE CORS FOR GITHUB PAGES
+// ----------------------------
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
@@ -11,24 +13,30 @@ app.use((req, res, next) => {
   next();
 });
 
-// Make HLS folders
-if (!fs.existsSync("hls")) fs.mkdirSync("hls");
-["ss2", "ten3", "ss1_4k"].forEach(dir => {
-  if (!fs.existsSync(`hls/${dir}`)) fs.mkdirSync(`hls/${dir}`);
-});
-
-// CHANNEL LIST (YOUR STREAMS)
+// ----------------------------
+// CHANNEL LIST (YOU OWN THESE)
+// ----------------------------
 const CHANNELS = {
-  ss2:    "http://87.255.35.150:18828", // Star Sports 2
-  ten3:   "http://87.255.35.150:18848", // Sony TEN 3 Hindi
-  ss1_4k: "http://sports-rkdyiptv.wasmer.app/play.php?id=71242" // STAR SPORTS 1 4K HINDI
+  ss2:      "http://87.255.35.150:18828",                 // Star Sports 2
+  ten3:     "http://87.255.35.150:18848",                 // Sony Ten 3 Hindi
+  ss1_4k:   "http://sports-rkdyiptv.wasmer.app/play.php?id=71242"  // Star Sports 1 4K (YOU OWN)
 };
 
-// Start FFmpeg for each channel
-function startFF(name, url) {
-  console.log("Starting:", name);
+// ----------------------------
+// MAKE HLS DIRECTORIES
+// ----------------------------
+if (!fs.existsSync("hls")) fs.mkdirSync("hls");
+for (let ch in CHANNELS) {
+  if (!fs.existsSync(`hls/${ch}`)) fs.mkdirSync(`hls/${ch}`);
+}
 
-  const ff = spawn("ffmpeg", [
+// ----------------------------
+// START FFMPEG FOR EACH CHANNEL
+// ----------------------------
+function startChannel(name, url) {
+  console.log(`Starting channel: ${name}`);
+
+  const ffmpeg = spawn("ffmpeg", [
     "-i", url,
     "-c:v", "copy",
     "-c:a", "copy",
@@ -39,26 +47,31 @@ function startFF(name, url) {
     `hls/${name}/playlist.m3u8`
   ]);
 
-  ff.stderr.on("data", d => console.log(`[${name}]`, d.toString()));
-  ff.on("close", () => console.log(`Stopped: ${name}`));
+  ffmpeg.stderr.on("data", data => {
+    console.log(`[${name}] ${data}`);
+  });
+
+  ffmpeg.on("close", () => {
+    console.log(`FFmpeg stopped for: ${name}`);
+  });
 }
 
-Object.entries(CHANNELS).forEach(([name, url]) => startFF(name, url));
+Object.entries(CHANNELS).forEach(([name, url]) => startChannel(name, url));
 
-// Serve HLS
+// ----------------------------
+// STATIC FILE SERVING
+// ----------------------------
 app.use("/hls", express.static("hls"));
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>Live Channels Running</h1>
+    <h1>Channels Running</h1>
     <ul>
       <li><a href="/hls/ss2/playlist.m3u8">Star Sports 2</a></li>
-      <li><a href="/hls/ten3/playlist.m3u8">Sony TEN 3 - Hindi</a></li>
-      <li><a href="/hls/ss1_4k/playlist.m3u8">Star Sports 1 4K - Hindi</a></li>
+      <li><a href="/hls/ten3/playlist.m3u8">Sony Ten 3 Hindi</a></li>
+      <li><a href="/hls/ss1_4k/playlist.m3u8">Star Sports 1 4K Hindi</a></li>
     </ul>
   `);
 });
 
-app.listen(3000, () =>
-  console.log("HLS Server running on port 3000")
-);
+app.listen(3000, () => console.log("Server running on port 3000"));
